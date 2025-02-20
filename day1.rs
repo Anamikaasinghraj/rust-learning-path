eee326c812921feea9308139087417929a157c52
Anamika Singh Raj <anamikasinghrajraj@gmail.com>
2025-02-21 03:08:25 +0530

    Day1.rs

---
+++ day1.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("hello, anamika!");
+}
+
+
+//snake_case: hello_world - rust follow
+//camelCase: helloWorld
+
+//Data Types- i)scalar types:- integers, Floating-Point, etc.  ii)compound types:- primitive types, complex types.
+
+fn main() {
+    let num:u8 = 12;
+    println!("this value stored in num{}",num);
+}
+
+// u8 stand for unsigned integer.
